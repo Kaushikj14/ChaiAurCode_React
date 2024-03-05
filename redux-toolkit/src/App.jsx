d607d8f7a89cdc34da692455a60a34e0e217bb9d
Kaushikj14 <kaushikjain1423@gmail.com>
2024-03-05 17:01:32 +0530

    Redux toolkit basics

---
+++ redux-toolkit/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import AddTodos from './components/AddTodos'
+import Todos from './components/Todos'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn About Redux</h1>
+      <AddTodos />
+      <Todos />
+    </>
+  )
+}
+
+export default App
